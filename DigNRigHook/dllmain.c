fac5de6c62b82e18c7686d4ebce11044bd43a5d8
ryanblucas <ryanl80020@gmail.com>
2026-06-11 13:40:42 -0500

    Add hook project

---
+++ DigNRigHook/dllmain.c
@@ -0,0 +1,28 @@
+#include <Windows.h>
+
+/* this is to resolve any linking issues with exports.def */
+
+void __stdcall FMOD_System_Update() {}
+void __stdcall FMOD_System_Create() {}
+void __stdcall FMOD_System_GetVersion() {}
+void __stdcall FMOD_System_Init() {}
+void __stdcall FMOD_Channel_SetVolume() {}
+void __stdcall FMOD_Channel_Stop() {}
+void __stdcall FMOD_Channel_GetVolume() {}
+void __stdcall FMOD_System_PlaySound() {}
+void __stdcall FMOD_System_CreateSound() {}
+void __stdcall FMOD_Channel_IsPlaying() {}
+
+BOOL APIENTRY DllMain(HMODULE module, DWORD reason_for_call, LPVOID reserved)
+{
+    switch (reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}

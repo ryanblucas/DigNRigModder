ce7498975086ec1bbd0cf36e6cc60f70eaa61581
ryanblucas <ryanl80020@gmail.com>
2026-06-26 20:42:03 -0500

    Updated event_queue to be more thread-safe

--- src/event_queue.c
+++ src/event_queue.c
@@ -5,6 +5,9 @@
 #include "event_queue.h"
 #include <Windows.h>
 
+#define IS_RUNNING_FROM_WINDOW() (GetCurrentThreadId() != main_thread_id)
+#define IS_RUNNING_FROM_CONSOLE() (GetCurrentThreadId() == main_thread_id)
+
 struct queued_event
 {
 	handle_generic_t handler;
@@ -18,9 +21,11 @@ static struct queued_event queue[16];
 static int top;
 
 static uint8_t main_arena[0x400];
-static size_t main_arena_top;
 static uint8_t window_arena[0x400];
-static size_t window_arena_top;
+static volatile LONG main_arena_top;
+static volatile LONG window_arena_top;
+
+static volatile LONG pending_main_arena_events;
 
 static DWORD main_thread_id;
 static HWND window;
@@ -37,13 +42,18 @@ void queue_set_window_handle(HWND wnd)
 
 void* queue_copy_data(const void* data, size_t size)
 {
-	uint8_t* arena = GetCurrentThreadId() == main_thread_id ? main_arena : window_arena;
-	size_t* arena_top = GetCurrentThreadId() == main_thread_id ? &main_arena_top : &window_arena_top;
+	if (IS_RUNNING_FROM_CONSOLE())
+	{
+		InterlockedIncrement(&pending_main_arena_events);
+	}
 
-	RUNTIME_ASSERT(*arena_top + size < sizeof main_arena);
-	memcpy(arena + *arena_top, data, size);
-	*arena_top += size;
-	return arena + *arena_top - size;
+	uint8_t* arena = IS_RUNNING_FROM_CONSOLE() ? main_arena : window_arena;
+	LONG arena_top = IS_RUNNING_FROM_CONSOLE() ? main_arena_top : window_arena_top;
+
+	RUNTIME_ASSERT(arena_top + size < sizeof main_arena);
+	memcpy(arena + arena_top, data, size);
+	InterlockedAdd(IS_RUNNING_FROM_CONSOLE() ? &main_arena_top : &window_arena_top, (LONG)size);
+	return arena + arena_top;
 }
 
 static inline void queue_lock_acquire(volatile LONG* lock)
@@ -59,13 +69,33 @@ static inline void queue_lock_release(volatile LONG* lock)
 	InterlockedExchange(lock, 0);
 }
 
+void queue_add_from_main(handle_generic_t handler, const void* data)
+{
+	if (IS_RUNNING_FROM_CONSOLE())
+	{
+		handler(data);
+		return;
+	}
+	queue_add(handler, data);
+}
+
+void queue_add_from_window(handle_generic_t handler, const void* data)
+{
+	if (IS_RUNNING_FROM_WINDOW())
+	{
+		handler(data);
+		return;
+	}
+	queue_add(handler, data);
+}
+
 void queue_add(handle_generic_t handler, const void* data)
 {
 	if (!handler)
 	{
 		return;
 	}
-	if (GetCurrentThreadId() == main_thread_id)
+	if (IS_RUNNING_FROM_CONSOLE())
 	{
 		debug_format("Adding event to window\n");
 		PostMessageW(window, QUEUE_MSG_MAIN_TO_WINDOW, (WPARAM)handler, (LPARAM)data);
@@ -79,7 +109,7 @@ void queue_add(handle_generic_t handler, const void* data)
 
 int queue_run(void)
 {
-	if (GetCurrentThreadId() != main_thread_id)
+	if (IS_RUNNING_FROM_WINDOW())
 	{
 		return 0;
 	}
@@ -92,6 +122,20 @@ int queue_run(void)
 		event.handler(event.data);
 	}
 	queue_lock_release(&queue_lock);
-	main_arena_top = 0;
+	InterlockedExchange(&window_arena_top, 0);
 	return res;
+}
+
+void queue_pop_window_event(void)
+{
+	if (IS_RUNNING_FROM_CONSOLE())
+	{
+		return;
+	}
+
+	InterlockedDecrement(&pending_main_arena_events);
+	if (InterlockedCompareExchange(&pending_main_arena_events, 0, 0) <= 0)
+	{
+		InterlockedExchange(&main_arena_top, 0);
+	}
 }
--- src/event_queue.h
+++ src/event_queue.h
@@ -18,9 +18,17 @@ void queue_set_window_handle(HWND wnd);
 
 /* copies data from parameter to be freed when ran. This is for passing a stack variable to queue_add */
 void* queue_copy_data(const void* data, size_t size);
+
+/* Queues event from main. If already on main, it will immediately call. */
+void queue_add_from_main(handle_generic_t handler, const void* data);
+/* Queues event from window. If already on window, it will immediately call. */
+void queue_add_from_window(handle_generic_t handler, const void* data);
 /* Queues event from seperate thread to be executed on the other. */
 void queue_add(handle_generic_t handler, const void* data);
+
 /* Clears and executes queue on main thread. Returns amount of events processed */
 int queue_run(void);
 /* Main thread to window thread is handled by sending a message to its Win32 
-   message queue; therefore, there is no respective queue_run_window function */
+   message queue; therefore, there is no respective queue_run_window function.
+   But, it still needs to know when to clear its arena, hence that is what this function does. */
+void queue_pop_window_event(void);
